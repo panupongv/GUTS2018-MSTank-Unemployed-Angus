6b4183911dc2060286c867820297d4386916ed24
= <->
2018-10-13 02:59:37 +0100

    make sending message works

--- src/tryconnect.js
+++ src/tryconnect.js
@@ -5,7 +5,7 @@ var client = new net.Socket();
 
 client.connect(8052, '127.0.0.1', function() {
     console.log('Connected');
-    cmd = "{'Name':'EmployedAngus'}";
+    cmd = '{"Name":"UnemployedAngus"}';
 
     //version 0
     // uia = new Uint8Array(2);
@@ -24,16 +24,26 @@ client.connect(8052, '127.0.0.1', function() {
     // b3 = Buffer.concat([b1,b2])
     // console.log(client.write(b3));
 
-
     //version 3
-    uia = new Uint8Array(2);
-    uia[0] = 0
-    uia[1] = 0
-    for(var i = 0; i < 10; i++)
-        console.log(client.write(uia.toString()));
+    // uia = new Uint8Array(2);
+    // uia[0] = 0
+    // uia[1] = 0
+    // for(var i = 0; i < 10; i++)
+    //     console.log(client.write(uia.toString()));
 
-    // client.write(new Buffer([1,cmd.length]))
+    //version 4
+    // uia = new Uint8Array([1, 21, 123, 34, 78, 97, 109, 101, 34, 58, 32, 34, 82, 97, 110, 100, 111, 109, 66, 111, 116, 34, 125]);
+    // console.log(client.write(uia));
 
+    //version 5
+    // uia = new Uint8Array([1, 21, 123, 34, 78, 97, 109, 101, 34, 58, 32, 34, 82, 97, 110, 100, 111, 109, 66, 111, 116, 34, 125]);
+    // client.write(uia.slice(0,2));
+    // client.write(uia.slice(2));
+
+    //version 6
+    uia = new Uint8Array([1, cmd.length+1]);
+    client.write(uia);
+    client.write(utf8.encode(cmd));
 
     // client.write(utf8.encode(cmd))
 
